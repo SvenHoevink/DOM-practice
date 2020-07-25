10acb6ea201c6b1a3100a21944c7c3c5872fae9f
Sven Hoevink <svenhoevink@gmail.com>
2020-07-25 09:33:49 -0800

    removed hardcoded buttons & restarted JS

--- script.js
+++ script.js
@@ -1,58 +1,34 @@
-var input = document.querySelector("#userinput");
-var btn = document.querySelector("#enter");
-var ul = document.querySelector("ul");
-var item;
-var btn;
+let input = document.querySelector("#userinput");
+let button = document.querySelector("#enter");
+let ul = document.querySelector("ul");
+let items = document.querySelectorAll(".item");
 
-function inputValue(){
-	return input.value.length;
+let inputLength = () => input.value.length;
+
+for (i = 0; i < items.length; i++) {
+  let button = document.createElement("button");
+  button.appendChild(document.createTextNode("delete"));
+  items[i].appendChild(button);
 }
 
+let addItem = () => {
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+  let button = document.createElement("button");
+  button.appendChild(document.createTextNode("Delete"));
+  li.appendChild(button);
+};
 
-function addItem(){
-	item = document.createElement("li");
-	item.appendChild(document.createTextNode(input.value));
-	ul.appendChild(item);
-	input.value = "";
+button.addEventListener("click", () => {
+  if (inputLength() > 0) {
+    addItem();
+  }
+});
 
-}
-
-function deleteBtn(){
-	var delBtn = document.createElement("button");
-	delBtn.appendChild(document.createTextNode("delete"));
-	btn = document.querySelector("button");
-	item.appendChild(delBtn);
-	delBtn.addEventListener("click", function(){
-	item.remove();
-	})
-	btn.addEventListener("click", function(){
-		item.remove();
-	})
-	
-}
-
-
-function btnPress(){
-	if(inputValue() > 0){
-		addItem();
-		deleteBtn();
-	}
-}
-
-function enterPress(e){
-	if(inputValue() > 0 && e.keyCode === 13){
-		addItem();
-		deleteBtn();
-	}
-}
-
-function checkItem(){
-item.addEventListener("click", function(){
-	item.classList.toggle("done");
-})
-}
-
-btn.addEventListener("click", btnPress);
-
-input.addEventListener("keypress", enterPress);
-
+input.addEventListener("keypress", (e) => {
+  if (inputLength() > 0 && e.keyCode === 13) {
+    addItem();
+  }
+});
